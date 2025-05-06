d1c1e8df26c6e2e942e70905363a9471d51096a6
canyadigit29 <canyadigit29@gmail.com>
2025-05-05 22:39:36 -0400

    Update __init__.py

--- app/api/project_ops/__init__.py
+++ app/api/project_ops/__init__.py
@@ -1,4 +1,9 @@
 # package marker
 from . import project
-from . import background_tasks
 from . import session_log
+
+def load_background_tasks():
+    from . import background_tasks  # import inside a function to avoid circular import
+    return background_tasks
+
+# You can later call this function to load background_tasks when needed
